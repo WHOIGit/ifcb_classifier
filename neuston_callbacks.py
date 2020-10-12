9bab3ed6cc90dc308bc5eb4b93d639daaf5ab74a
Sidney Batchelder <sbatchelder@whoi.edu>
2020-10-12 15:26:45 -0400

    save_run_results() restructured to also work outside callback.

--- neuston_callbacks.py
+++ neuston_callbacks.py
@@ -149,8 +149,90 @@ class SaveValidationResults(ptl.callbacks.base.Callback):
 
 
 ## Running ##
+def save_run_results(input_images, output_scores, class_labels, timestamp, outdir, outfile, bin_id=None, model_id=None):
+    output_classranks = np.max(output_scores, axis=1)
+    output_classes = np.argmax(output_scores, axis=1)
 
-class SaveRunResults(ptl.callbacks.base.Callback):
+    assert output_scores.shape[0] == len(output_classes), 'wrong number inputs-to-outputs'
+    assert output_scores.shape[1] == len(class_labels), 'wrong number of class labels'
+
+    results = dict(model_id=model_id,
+                   timestamp=timestamp,
+                   class_labels=class_labels,
+                   input_images=input_images,
+                   output_classes=output_classes,
+                   output_scores=output_scores)
+
+    outfile = os.path.join(outdir, outfile)
+    if bin_id:
+        results['bin'] = bin_id
+        results['roi_numbers'] = [ifcb.Pid(img).target for img in input_images]
+        outfile = outfile.format(bin=bin_id)
+    else:  # ImageDataset
+        pass
+    os.makedirs(os.path.dirname(outfile), exist_ok=True)
+
+    if outfile.endswith('.json'): _save_run_results_json(outfile, results)
+    if outfile.endswith('.mat'): _save_run_results_mat(outfile, results)
+    if outfile.endswith('.h5'): _save_run_results_hdf(outfile, results)
+
+def _save_run_results_json(outfile, results):
+    # results: model_id timestamp class_labels (bin + roi_numbers)
+    #          input_images output_classes output_scores
+
+    output = dict(model_id = results['model_id'],
+                  timestamp = results['timestamp'],
+                  class_labels = results['class_labels'],
+                  output_scores = results['output_scores'].tolist(),
+                  output_classes = results['output_classes'].tolist() )
+    if 'bin' in results:
+        output['bin'] = results['bin']
+        output['roi_numbers'] = results['roi_numbers']
+    else:
+        output['input_images'] = results['input_images']
+
+    with open(outfile, 'w') as f:
+        json.dump(output, f)
+
+def _save_run_results_mat(outfile, results):
+    # results: model_id timestamp class_labels (bin + roi_numbers)
+    #          input_images output_classes output_scores
+
+    output=dict()
+    # increment indexes, since matlab is not zero-indexed
+    output['output_classes'] = results['output_classes'].astype('u4')+1
+    output['model_id'] = results['model_id']
+    output['timestamp'] = results['timestamp']
+    output['output_scores'] = results['output_scores'].astype('f4')
+    output['class_labels'] = np.asarray(results['class_labels'], dtype='object')
+    if 'bin' in results:
+        output['bin'] = results['bin']
+        output['roi_numbers'] = results['roi_numbers']#.astype('u4') #not numpy yet
+    else:
+        output['input_images'] = np.asarray(results['input_images'], dtype='object')
+
+    savemat(outfile, output, do_compression=True)
+
+def _save_run_results_hdf(outfile, results):
+    # results: model_id timestamp class_labels (bin + roi_numbers)
+    #          input_images output_classes output_scores
+
+    with h5.File(outfile, 'w') as f:
+        meta = f.create_dataset('metadata', data=h5.Empty('f'))
+        meta.attrs['model_id'] = results['model_id']
+        meta.attrs['timestamp'] = results['timestamp']
+        f.create_dataset('output_classes', data=results['output_classes'], compression='gzip', dtype='float16')
+        f.create_dataset('output_scores', data=results['output_scores'], compression='gzip', dtype='float16')
+        f.create_dataset('class_labels', data=np.string_(results['class_labels']), compression='gzip', dtype=h5.string_dtype())
+        if results['bin']:
+            meta.attrs['bin'] = results['bin']
+            f.create_dataset('roi_numbers', data=results['roi_numbers'], compression='gzip', dtype='float16')
+        else:
+            f.create_dataset('input_images', data=np.string_(results['input_images']), compression='gzip', dtype=h5.string_dtype())
+
+
+
+class SaveTestResults(ptl.callbacks.base.Callback):
 
     def __init__(self, outdir, outfile, timestamp):
         self.outdir = outdir
@@ -158,93 +240,16 @@ class SaveRunResults(ptl.callbacks.base.Callback):
         self.timestamp = timestamp
 
     def on_test_end(self, trainer, pl_module):
-        class_labels = pl_module.hparams.classes
 
         rr = trainer.callback_metrics['RunResults']
         # RunResult: inputs, outputs, bin_id
 
         output_scores = rr.outputs
-
-        output_classranks = np.max(output_scores, axis=1)
-        output_classes = np.argmax(output_scores, axis=1)
-
-        assert output_scores.shape[0] == len(output_classes), 'wrong number inputs-to-outputs'
-        assert output_scores.shape[1] == len(class_labels), 'wrong number of class labels'
-
-        results = dict(model_id=pl_module.hparams.model_id,
-                       timestamp=self.timestamp,
-                       class_labels=class_labels,
-                       input_images=rr.inputs,
-                       output_classes=output_classes,
-                       output_scores=output_scores)
-
-        outfile = os.path.join(self.outdir, self.outfile)
+        input_images = rr.inputs
+        model_id = pl_module.hparams.model_id
+        class_labels = pl_module.hparams.classes
         dataset = pl_module.test_dataloader().dataset
-        if isinstance(dataset, IfcbBinDataset):
-            results['bin'] = str(dataset.bin.pid)
-            results['roi_numbers'] = [ifcb.Pid(img).target for img in rr.inputs]
-            outfile = outfile.format(bin=dataset.bin.pid)
-        else: # ImageDataset
-            pass
-        os.makedirs(os.path.dirname(outfile),exist_ok=True)
-        self.save_run_results(outfile, results)
+        bin_id = str(dataset.bin.pid) if isinstance(dataset, IfcbBinDataset) else None
 
-    def save_run_results(self, outfile, results):
-        if outfile.endswith('.json'): self._save_run_results_json(outfile, results)
-        if outfile.endswith('.mat'): self._save_run_results_mat(outfile, results)
-        if outfile.endswith('.h5'): self._save_run_results_hdf(outfile, results)
-
-    def _save_run_results_json(self, outfile, results):
-        # results: model_id timestamp class_labels (bin + roi_numbers)
-        #          input_images output_classes output_scores
-
-        output = dict(model_id = results['model_id'],
-                      timestamp = results['timestamp'],
-                      class_labels = results['class_labels'],
-                      output_scores = results['output_scores'].tolist(),
-                      output_classes = results['output_classes'].tolist() )
-        if 'bin' in results:
-            output['bin'] = results['bin']
-            output['roi_numbers'] = results['roi_numbers']
-        else:
-            output['input_images'] = results['input_images']
-
-        with open(outfile, 'w') as f:
-            json.dump(output, f)
-
-    def _save_run_results_mat(self, outfile, results):
-        # results: model_id timestamp class_labels (bin + roi_numbers)
-        #          input_images output_classes output_scores
-
-        output=dict()
-        # increment indexes, since matlab is not zero-indexed
-        output['output_classes'] = results['output_classes'].astype('u4')+1
-        output['model_id'] = results['model_id']
-        output['timestamp'] = results['timestamp']
-        output['output_scores'] = results['output_scores'].astype('f4')
-        output['class_labels'] = np.asarray(results['class_labels'], dtype='object')
-        if 'bin' in results:
-            output['bin'] = results['bin']
-            output['roi_numbers'] = results['roi_numbers']#.astype('u4') #not numpy yet
-        else:
-            output['input_images'] = np.asarray(results['input_images'], dtype='object')
-
-        savemat(outfile, output, do_compression=True)
-
-    def _save_run_results_hdf(self, outfile, results):
-        # results: model_id timestamp class_labels (bin + roi_numbers)
-        #          input_images output_classes output_scores
-
-        with h5.File(outfile, 'w') as f:
-            meta = f.create_dataset('metadata', data=h5.Empty('f'))
-            meta.attrs['model_id'] = results['model_id']
-            meta.attrs['timestamp'] = results['timestamp']
-            f.create_dataset('output_classes', data=results['output_classes'], compression='gzip', dtype='float16')
-            f.create_dataset('output_scores', data=results['output_scores'], compression='gzip', dtype='float16')
-            f.create_dataset('class_labels', data=np.string_(results['class_labels']), compression='gzip', dtype=h5.string_dtype())
-            if results['bin']:
-                meta.attrs['bin'] = results['bin']
-                f.create_dataset('roi_numbers', data=results['roi_numbers'], compression='gzip', dtype='float16')
-            else:
-                f.create_dataset('input_images', data=np.string_(results['input_images']), compression='gzip', dtype=h5.string_dtype())
+        save_run_results(input_images, output_scores, class_labels, self.timestamp, self.outdir, self.outfile, bin_id, model_id)
 
